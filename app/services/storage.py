78bcb7cbcacb7840a7aa8f3c30b47094d44be882
Xavier A P <xavierap058@gmail.com>
2026-02-14 00:40:30 +0530

    Updating s3 region name

--- app/services/storage.py
+++ app/services/storage.py
@@ -5,6 +5,7 @@ from fastapi import UploadFile
 
 s3_client = boto3.client(
     's3',
+    region_name="eu-north-1",
     aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
     aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY
 )
